f436e48fbc92ec772a2d2b50ba872843589a1dbe
Gezow <kokenyesdanone@gmail.com>
2021-09-21 11:38:45 +0200

    cooInput and converter func from other projects

--- src/main/java/com/codecool/battleship/input/Input.java
+++ src/main/java/com/codecool/battleship/input/Input.java
@@ -26,4 +26,62 @@ public class Input {
         }
     }
 
+    public int [] cooInput(){
+        char[] abc = Util.getLetters(10, false);
+        String move = userInput.nextLine();
+        char [] startingCord = move.toCharArray();
+        if (!validInput(startingCord)){
+            //wrong input message
+            return cooInput();
+        }
+        int firstCoo = convertFirstCord(startingCord[0], abc);
+        int secCoo = convertSecondCord(startingCord, abc);
+        if (secCoo<0 || firstCoo<0){
+            //wrong input message
+            return cooInput();
+        }
+        int[] startingCoo = new int[2];
+        startingCoo[0] = firstCoo;
+        startingCoo[1] = secCoo;
+        return startingCoo;
+    }
+
+    public int convertFirstCord(char cord, char[] abc){
+        cord = Character.toUpperCase(cord);
+        int firstCord = 0;
+        boolean found = false;
+        for (int i=0; i< abc.length; i++){
+            if (abc[i] == cord){
+                firstCord = i;
+                found = true;
+            }
+        }
+        if (found){
+            return firstCord;
+        }
+        return -1;
+    }
+
+
+    public int convertSecondCord(char [] cords, char[] abc){
+        String convertCord;
+        if (cords.length >2){
+            convertCord = String.valueOf(cords[1]) + cords[2];
+        }else{
+            convertCord = String.valueOf(cords[1]);
+        }
+        int coo = Integer.parseInt(convertCord)-1;
+        if (coo<abc.length){
+            return coo;
+        }
+        return -1;
+    }
+
+    public boolean validInput(char [] coords){
+        if (coords.length<2 || coords.length>3){
+            return false;
+        }else if (coords.length == 3){
+            return Character.isDigit(coords[1]) && Character.isDigit(coords[2]);
+        }else return Character.isDigit(coords[1]);
+    }
 }
