19b00c09a0b426b094791bbbfed9037766b31a0d
Gezow <kokenyesdanone@gmail.com>
2021-09-21 16:08:03 +0200

    bugfix isValidCoo func

--- src/main/java/com/codecool/battleship/board/BoardFactory.java
+++ src/main/java/com/codecool/battleship/board/BoardFactory.java
@@ -1,29 +1,28 @@
 package com.codecool.battleship.board;
 
 import com.codecool.battleship.input.Input;
+import com.codecool.battleship.player.Player;
+import com.codecool.battleship.ship.Ship;
+import com.codecool.battleship.square.Square;
+
+import java.util.List;
 
 public class BoardFactory {
 
     //theoretically works with the smallest ships
-    public void placement(Player player, Input input) {
-        ships = player.ships;
+    public void manualPlacement(Player player, Board board, Input input) {
+        List<Ship> ships = player.getShips();
         for (Ship ship : ships) {
             int[] startingCoo = input.cooInput();
-            if (isValidCoo(startingCoo)) {
-                if (ship.location.length = 1) {
-                    ship.location[0].x = startingCoo[0];
-                    ship.location[1].x = startingCoo[1];
+            if (isValidCoo(startingCoo, board)) {
+                // if the ship has one square, then place it
                 }
             }
         }
     }
 
-    public boolean isValidCoo(int[] inputCoo) {
-        for (Square square : board.ocean) {
-            if (square.x = inputCoo[0] || square.y = inputCoo[1]) {
-                return true;
-            }
-        }
-        return false;
+    public boolean isValidCoo(int[] inputCoo, Board board) {
+        int length = board.getOcean().length;
+        return 0 <= inputCoo[0] && 0 <= inputCoo[1] && inputCoo[0] <= length && inputCoo[1] <= length;
     }
 }
