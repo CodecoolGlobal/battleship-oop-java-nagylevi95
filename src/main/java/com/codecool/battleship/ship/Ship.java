69a7e9851a2cf51c983d06439135bd935a74ebcb
troll92and <troll92and@gmail.com>
2021-09-21 11:47:16 +0200

    modify the ship class fields

--- src/main/java/com/codecool/battleship/ship/Ship.java
+++ src/main/java/com/codecool/battleship/ship/Ship.java
@@ -1,30 +1,42 @@
 package com.codecool.battleship.ship;
 
+import com.codecool.battleship.squares.Square;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Stack;
 
 public class Ship {
-    List<Square> position = new List<Square>();
-    int life;
-    String Player;
+    private List<Square> position;
+    private ShipType shipType;
+    private Player player;
+    private Boolean isSunk;
 
-    public Ship(List<Square> position) {
+    public Ship(List<Square> position, ShipType shipType, Player player, Boolean isSunk) {
         this.position = position;
+        this.shipType = shipType;
+        this.player = player;
+        this.isSunk = false;
     }
 
-    public int getLife() {
-        return life;
+
+
+    public Player getPlayer() {
+        return player;
+    }
+    
+
+    public Boolean getSunk() {
+        return isSunk;
     }
 
-    public void setLife(int life) {
-        this.life = life;
+    public void setSunk(Boolean sunk) {
+        isSunk = sunk;
     }
 
-    public String getPlayer() {
-        return Player;
+    public ShipType getShipType() {
+        return shipType;
     }
 
-    public void setPlayer(String player) {
-        Player = player;
-    }
 
 }
