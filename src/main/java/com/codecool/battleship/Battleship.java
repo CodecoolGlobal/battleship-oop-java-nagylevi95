b1ef590e1c983a6bd3477ae9dafff7e606af98c7
nagylevi95 <levente.nagy.ev@gmail.com>
2021-09-21 13:00:24 +0200

    change Battleship.java: get rid of statics, rebuild for testing

--- src/main/java/com/codecool/battleship/Battleship.java
+++ src/main/java/com/codecool/battleship/Battleship.java
@@ -4,38 +4,42 @@ import com.codecool.battleship.display.Display;
 import com.codecool.battleship.input.Input;
 
 public class Battleship {
-    private static Display display;
-    private static Input inputs;
-    private static Game game;
-    public static boolean run;
+    private Display display;
+    private Input inputs;
+//    private Game game;
+    public boolean run;
 
     private Battleship() {
         display = new Display();
         inputs = new Input();
-        game = new Game();
+//        game = new Game();
         run = true;
     }
 
-    private static void mainMenu(){
-        display.displayMenu();
-        int chosenMode = inputs.menuInput();
-        switch (chosenMode){
-            case 0:
-                run = false;
-                break;
-            case 1:
-                game.newGame();
-            case 2:
-                display.highScore();
-            default:
-                display.wrongInput();
-                mainMenu();
+    private void mainMenu(){
+        while (run){
+            display.displayMenu();
+            int chosenMode = inputs.userInt();
+            switch (chosenMode){
+                case 0:
+                    run = false;
+                    break;
+                case 1:
+                    System.out.println("new game");
+//                    game.newGame();
+                    break;
+                case 2:
+                    System.out.println("high score");
+//                    display.highScore();
+                    break;
+                default:
+                    display.wrongInput();
+                    mainMenu();
+            }
         }
     }
 
     public static void main(String args[]){
-        while (run){
-            mainMenu();
-        }
+        new Battleship().mainMenu();
     }
 }
