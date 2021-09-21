342624e953e68a393eb121e9666cb3469f6fdb2b
Gezow <kokenyesdanone@gmail.com>
2021-09-21 11:19:21 +0200

    userInt and isInt func

--- src/main/java/com/codecool/battleship/input/Input.java
+++ src/main/java/com/codecool/battleship/input/Input.java
@@ -5,4 +5,25 @@ import com.codecool.battleship.Util;
 import java.util.Scanner;
 
 public class Input {
+
+    Scanner userInput = new Scanner(System.in);
+
+    public int userInt() {
+        String input = "";
+        while (isInt(input)) {
+            input = userInput.nextLine();
+        }
+        return Integer.parseInt(input);
+    }
+
+    private boolean isInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (
+                NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
