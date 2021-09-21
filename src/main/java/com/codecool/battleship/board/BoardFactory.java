ee70e767fd4a22fd57ca609e895f5620093d9fb9
Gezow <kokenyesdanone@gmail.com>
2021-09-21 16:23:57 +0200

    bugfix BoardFactory

--- src/main/java/com/codecool/battleship/board/BoardFactory.java
+++ src/main/java/com/codecool/battleship/board/BoardFactory.java
@@ -4,6 +4,7 @@ import com.codecool.battleship.input.Input;
 import com.codecool.battleship.player.Player;
 import com.codecool.battleship.ship.Ship;
 import com.codecool.battleship.square.Square;
+import com.codecool.battleship.square.SquareStatus;
 
 import java.util.List;
 
@@ -12,17 +13,22 @@ public class BoardFactory {
     //theoretically works with the smallest ships
     public void manualPlacement(Player player, Board board, Input input) {
         List<Ship> ships = player.getShips();
+        Square[][] ocean = board.getOcean();
         for (Ship ship : ships) {
             int[] startingCoo = input.cooInput();
-            if (isValidCoo(startingCoo, board)) {
-                // if the ship has one square, then place it
-                }
+            if (isValidCoo(startingCoo, ocean) && isEmptyField(startingCoo, ocean)) {
+                // place small ship here
             }
         }
     }
 
-    public boolean isValidCoo(int[] inputCoo, Board board) {
-        int length = board.getOcean().length;
+    public boolean isValidCoo(int[] inputCoo, Square[][] ocean) {
+        int length = ocean.length;
         return 0 <= inputCoo[0] && 0 <= inputCoo[1] && inputCoo[0] <= length && inputCoo[1] <= length;
+    }
+
+    public boolean isEmptyField(int[] inputCoo, Square[][] ocean) {
+        Square targetSquare = ocean[inputCoo[0]][inputCoo[1]];
+        return (targetSquare.getSquareStatus() == SquareStatus.EMPTY);
     }
 }
