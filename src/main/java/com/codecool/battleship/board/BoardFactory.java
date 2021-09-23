85a631927c68cf546e6c7afe2b295b72d21703fc
Gezow <kokenyesdanone@gmail.com>
2021-09-23 11:58:06 +0200

    refactor BoardFactory - mob programming

--- src/main/java/com/codecool/battleship/board/BoardFactory.java
+++ src/main/java/com/codecool/battleship/board/BoardFactory.java
@@ -21,37 +21,46 @@ public class BoardFactory {
         List<Square> positions;
         Square[][] ocean = board.getOcean();
         for (ShipType shipType : ShipType.values()) {
-            display.providePlacementCoo(shipType);
-            positions = placeShips(ocean, shipType, input);
+            board.display();
+            positions = placeShips(ocean, shipType);
+            markShips(positions);
             ships.add(new Ship(positions, shipType, player));
 
         }
         player.setShips(ships);
     }
 
-    private List<Square> placeShips(Square[][] ocean, ShipType shipType, Input input) {
+    private List<Square> placeShips(Square[][] ocean, ShipType shipType) {
+        display.providePlacementCoo(shipType);
         int[] firstCoo = input.userCoo();
         List<Square> shipPositions = new LinkedList<>();
-        if (!isEmptyField(firstCoo, ocean) && !isValidCoo(firstCoo, ocean)) {
-            return placeShips(ocean, shipType, input);
+        if (!isEmptyField(firstCoo, ocean) || !isValidCoo(firstCoo, ocean)) {
+            display.wrongInput();
+            return placeShips(ocean, shipType);
         }
         display.provideDir();
         int[] direction = input.getDirection();
         Square targetSquare = ocean[firstCoo[0]][firstCoo[1]];
-        targetSquare.setSquareStatus(SquareStatus.SHIP);
-        shipPositions.add(targetSquare);
         int i = 1;
+        int[] nextCoo = new int[2];
         while (i < shipType.getLength()) {
-            placeShip(ocean, firstCoo, shipPositions, direction);
-            i++;
+            nextCoo[0] = firstCoo[0] + direction[0]*i;
+            nextCoo[1] = firstCoo[1] + direction[1]*i;
+            if (isValidCoo(nextCoo, ocean) && isEmptyField(nextCoo,ocean)) {
+                placeShip(ocean, nextCoo, shipPositions);
+                i++;
+            }else {
+                display.wrongInput();
+                return placeShips(ocean, shipType);
+            }
         }
+        shipPositions.add(targetSquare);
         return shipPositions;
     }
 
-    private void placeShip(Square[][] ocean, int[] firstCoo, List<Square> shipPositions, int[] direction) {
+    private void placeShip(Square[][] ocean, int[] nextCoo, List<Square> shipPositions) {
         Square targetSquare;
-        targetSquare = ocean[firstCoo[0] + direction[0]][firstCoo[1] + direction[1]];
-        targetSquare.setSquareStatus(SquareStatus.SHIP);
+        targetSquare = ocean[nextCoo[0]][nextCoo[1]];
         shipPositions.add(targetSquare);
     }
 
@@ -63,7 +72,7 @@ public class BoardFactory {
 
     public boolean isValidCoo(int[] inputCoo, Square[][] ocean) {
         int length = ocean.length;
-        return 0 <= inputCoo[0] && 0 <= inputCoo[1] && inputCoo[0] <= length && inputCoo[1] <= length;
+        return 0 <= inputCoo[0] && 0 <= inputCoo[1] && inputCoo[0] < length && inputCoo[1] < length;
     }
 
     public boolean isEmptyField(int[] inputCoo, Square[][] ocean) {
